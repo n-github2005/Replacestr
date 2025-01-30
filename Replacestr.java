5434263046f69ad04c7ce11ff037cf1d5ef60423
n-github2005 <kadudulanamitha@gmail.com>
2025-01-30 22:12:12 +0530

    Add files via upload

---
+++ Replacestr.java
@@ -0,0 +1,7 @@
+public class Replacestr {
+    public static void main(String[] args) {
+        String str="Hello World";
+        String replacedstr=str.replace('e','i');
+        System.out.println("replaced string:"+replacedstr);
+    }
+}
